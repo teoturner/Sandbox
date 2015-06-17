160f7bd0d46d0ce55ba6414567ca8b34b144081a
Klaus Stefan Gerber <stefan.gerber@turneo.com>
2015-06-17 13:57:05 +0200

    Adding functions for compiling constructors (with Unit Tests)

---
+++ Code/Tests/ActivatorTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.ObjectModel;
+
+namespace Sandbox.Tests
+{
+    [TestClass]
+    public class ActivatorTests
+    {
+        [TestMethod]
+        public void CreateGenericCollectionOfString()
+        {
+            Object activatedObject = Sandbox.Functions.Activator.CompileDefaultConstructorToInstanceActivator(typeof(Collection<String>))();
+
+            Assert.IsNotNull(activatedObject);
+            Assert.IsInstanceOfType(activatedObject, typeof(Collection<String>));
+        }
+
+        [TestMethod]
+        public void CreateGenericListOfString()
+        {
+            Object activatedObject = Sandbox.Functions.Activator.CompileDefaultConstructorToInstanceActivator(typeof(List<String>))();
+
+            Assert.IsNotNull(activatedObject);
+            Assert.IsInstanceOfType(activatedObject, typeof(Collection<String>));
+        }
+    }
+}
